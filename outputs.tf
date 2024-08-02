2cf86df7dc3531ae11734f0f35dad1d8f0e1497c
sanjay-aws <101187088+sanjay-aws@users.noreply.github.com>
2024-08-02 17:54:54 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,21 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat.id
+}
+
+output "load_balancer_dns_name" {
+  value = aws_lb.main.dns_name
+}
+
+      
