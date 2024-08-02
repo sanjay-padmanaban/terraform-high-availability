eb00b1328938f9e522f03a0acc9c1ecaafd36027
sanjay-aws <101187088+sanjay-aws@users.noreply.github.com>
2024-08-02 17:53:58 +0530

    Create app-loadbalancer.tf

---
+++ app-loadbalancer.tf
@@ -0,0 +1,34 @@
+resource "aws_lb" "main" {
+  name               = "${var.project_name}-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = aws_subnet.public.*.id
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "${var.project_name}-targets"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
