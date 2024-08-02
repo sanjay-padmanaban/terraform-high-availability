e3b389dc1cab154678f84e924dde5e96e7e05486
sanjay-aws <101187088+sanjay-aws@users.noreply.github.com>
2024-08-02 17:53:27 +0530

    Create security-groups.tf

---
+++ security-groups.tf
@@ -0,0 +1,45 @@
+# Security Group for Load Balancer
+resource "aws_security_group" "lb_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-lb-sg"
+  }
+}
+
+# Security Group for EC2 Instances
+resource "aws_security_group" "instance_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-instance-sg"
+  }
+}
