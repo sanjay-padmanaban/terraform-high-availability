a856b4170011f16298da91b42a4838f61be7082d
sanjay-aws <101187088+sanjay-aws@users.noreply.github.com>
2024-08-05 17:21:07 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.project_name}-vpc"
   }
-}
-
-provider "aws" {
-  region = var.region
 }
